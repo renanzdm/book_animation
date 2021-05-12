b43ce19fd6d92fac01ef97a07cfe986caa9506e7
renanzdm <renanzdm@gmail.com>
2021-05-11 21:47:09 -0300

    .

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:book_anim/app/modules/home/home_store.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({this.title = "Home"});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends ModularState<HomePage, HomeStore> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Container(
+        child: PageView.builder(itemBuilder: (context,index){
+          return Container();
+        }),
+      ),
+    );
+  }
+}
---
+++ lib/app/modules/model/book_model.dart
@@ -0,0 +1,15 @@
+
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+
+class BookModel {
+  String title;
+  String subTitle;
+  FileImage image;
+  BookModel({
+    required this.title,
+    required this.subTitle,
+    required this.image,
+  });
+}
